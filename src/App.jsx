1020f0bfe564d03735090ce1b8f8204f42114aae
chrismaynardampon <cmdampon@addu.edu.ph>
2024-10-01 19:20:07 +0800

    checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import ProductTable from './components/ProductTable';
 import SearchBar from './components/SearchBar';
 
 const products = [
-  {id: 1, name: 'Tennis', price: 49.9, type: 1},
-  {id: 2, name: 'Badminton', price: 99.9, type: 1},
-  {id: 3, name: 'Basketball', price: 29.9, type: 1},
+  {id: 1, name: 'Tennis', price: 49.9, type: 1, stock: 100},
+  {id: 2, name: 'Badminton', price: 99.9, type: 1, stock: 21},
+  {id: 3, name: 'Basketball', price: 29.9, type: 1, stock: 0},
 
-  {id: 4, name: 'Ipod Touch', price: 99.9, type: 2},
-  {id: 5, name: 'Iphone 5', price: 399.9, type: 2},
-  {id: 6, name: 'Nexus 7', price: 199.9, type: 2},
+  {id: 4, name: 'Ipod Touch', price: 99.9, type: 2, stock: 100},
+  {id: 5, name: 'Iphone 5', price: 399.9, type: 2, stock: 0},
+  {id: 6, name: 'Nexus 7', price: 199.9, type: 2, stock: 120},
 ];
 
 const headers = [
@@ -21,13 +21,19 @@ const headers = [
 
 function App() {
  const [query, setQuery] = useState("");
+ const [stockChecked, setStockChecked] = useState(false);
 
- const filteredProducts = products.filter(product => product.name.toLowerCase().includes(query.toLowerCase()));
+ const filteredProducts = products.filter(
+  // case 1
+  product => product.name.toLowerCase().includes(query.toLowerCase())
+  // case 2
+  && (stockChecked ? product.stock > 0 : true)
+);
 
   return (
     <div>
       <FilterProductTable>
-        <SearchBar query={query} setQuery={setQuery}/>
+        <SearchBar query={query} setQuery={setQuery} stockChecked={stockChecked} setStockChecked={setStockChecked} />
         <ProductTable headers={headers} products={filteredProducts}/>
       </FilterProductTable>
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
-const SearchBar = ({query, setQuery}) => {
+const SearchBar = ({query, setQuery, stockChecked, setStockChecked}) => {
   return (
     <div>
       <input type="text" name="search" id="search" value={query} onChange={(event) => setQuery(event.target.value) } />
       <div>
-      <input type="checkbox" name="checkbox" id="checkbox" />
+      <input type="checkbox" name="checkbox" id="checkbox" checked={stockChecked} onChange={(event) => setStockChecked(event.target.checked)}/>
       Only show products in stock
       </div>
     </div>
